1f6b99d7bdeb51584bb1384ae68ce9f6a90798e7
ajb-4 <alexbrown4711@gmail.com>
2023-09-30 18:39:55 -0400

    do user registration

---
+++ .history/backend/routes/api/users_20230930182033.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json({
+    message: "GET /api/users"
+  });
+});
+
+module.exports = router;
