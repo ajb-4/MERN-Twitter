8b49e278cf946c93dbb8050e7e2b140cb190c371
ajb-4 <alexbrown4711@gmail.com>
2023-09-30 21:19:38 -0400

    do tweets backend

--- .history/backend/routes/api/tweets_20230930210728.js
+++ .history/backend/routes/api/tweets_20230930210728.js
@@ -53,20 +53,4 @@ router.get('/:id', async (req, res, next) => {
   }
 });
 
-router.post('/', requireUser, validateTweetInput, async (req, res, next) => {
-    try {
-      const newTweet = new Tweet({
-        text: req.body.text,
-        author: req.user._id
-      });
-  
-      let tweet = await newTweet.save();
-      tweet = await tweet.populate('author', '_id username');
-      return res.json(tweet);
-    }
-    catch(err) {
-      next(err);
-    }
-  });
-
 module.exports = router;
